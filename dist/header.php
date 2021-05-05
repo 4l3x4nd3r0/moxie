b504f086bc8ca1ec6eace99e241386ee2411e82e
4l3x4nd3r0 <razvanpanaite21@gmail.com>
2021-05-05 15:21:06 +0300

    Am uitat ;

--- dist/header.php
+++ dist/header.php
@@ -26,12 +26,12 @@
                 <li><a href="index.php">Home</a></li>
                 <?php
                     if (isset($_SESSION["useruid"])) {
-                        echo "<li><a href="profile.php">Profile Page</a></li>"
-                        echo "<li><a href="includes/logout.inc.php">Logout</a></li>"
+                        echo "<li><a href="profile.php">Profile Page</a></li>";
+                        echo "<li><a href="includes/logout.inc.php">Logout</a></li>";
                     }
                     else {
-                        echo "<li><a href="signup.php">Sign Up</a></li>"
-                        echo "<li><a href="login.php">Login</a></li>"
+                        echo "<li><a href="signup.php">Sign Up</a></li>";
+                        echo "<li><a href="login.php">Login</a></li>";
                     }
                 ?>
             </ul>
