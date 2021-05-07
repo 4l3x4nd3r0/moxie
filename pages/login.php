fbb2e6f35389518a4c02ef52ea180bd1a4430fa7
4l3x4nd3r0 <razvanpanaite21@gmail.com>
2021-05-07 12:19:31 +0300

    Login Page

--- pages/login.php
+++ pages/login.php
@@ -1,15 +1,20 @@
 <div class="main">
-    <div class="body">
-        <section class="signup-form">
-            <div class="wrapper">
-            <h2>Log In</h2>
-            <div class="signup-form-form">
-                <form action="includes/login.inc.php" method="POST">
-                <input type="text" name="uid" placeholder="Username/Email...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="submit">Log In</button>
-                </form>
-            </div>
+        <section class="login">
+            <form action="includes/login.inc.php" method="POST">
+                <div class="title">
+                    <h2>Log In</h2><br>
+                </div>
+                <div class="title"><label>Username/Email
+                <input class="full" type="text" name="uid">
+                </label></div>
+                <div class="title">
+                    <label>Password
+                        <input class="full" type="password" name="pwd">
+                    </label>
+                </div>
+                <br>
+                <button class="btn-primary full" type="submit" name="submit">Log In</button>
+            </form>
             <?php
             if (isset($_GET["error"])) {
                 if ($_GET["error"] == "emptyinput") {
@@ -22,10 +27,5 @@
             ?>
             </div>
         </section>
-    </div>
 </div>
 
-<style>
-body {
-    background-color:#FBE8A6;
-}
