0cbba9b186f67142044fb76360ec1bbaa9eb61f0
Stefan Eduard <71097727+dotzzip@users.noreply.github.com>
2021-05-12 20:10:26 +0300

    database stuff

--- config/constants.php
+++ config/constants.php
@@ -2,10 +2,10 @@
 
 	//Create Constants to save Database Credentials
 	define('LOCALHOST', 'localhost');
-	define('USERNAME', 'root'); //Your Database username instead of 'root'
-	define('PASSWORD', ''); //Your Database Password instead of null/empty
-	define('DBNAME', 'mlb2018'); //Your Database Name if it's not 'mlb2018'
+	define('USERNAME', 'id16778245_root'); //Your Database username instead of 'root'
+	define('PASSWORD', '2WhncgAZr8]GI@r7'); //Your Database Password instead of null/empty
+	define('DBNAME', 'id16778245_mlb2018'); //Your Database Name if it's not 'mlb2018'
 
-	define('SITEURL', 'http://localhost/moxie/'); //Update the home URL of the project if you have changed port number or it's live on server
+	define('SITEURL', 'https://hermetic-reaches.000webhostapp.com/'); //Update the home URL of the project if you have changed port number or it's live on server
 
 ?>
