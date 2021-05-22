8b8ec73033b9bdc05890ef792dd078eb6feab1f5
Stefan Eduard <71097727+dotzzip@users.noreply.github.com>
2021-05-22 18:45:26 +0300

    center video on how to use

--- pages/howtouse.php
+++ pages/howtouse.php
@@ -3,9 +3,8 @@
   <div class="body">
 		<h2><?php echo $lang['howtoh'] ?></h2>
 		<br>
-      <br>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/eatSwW0QdSw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      
+
       </div>
    </div>
 </div>
