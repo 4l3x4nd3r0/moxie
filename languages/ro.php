9b186d170a880143d479a25263a93305505973ed
Stefan Eduard <71097727+dotzzip@users.noreply.github.com>
2021-05-26 19:04:43 +0300

    roumaine

--- languages/ro.php
+++ languages/ro.php
@@ -4,9 +4,9 @@
 		'meta_description' => 'Moxie, un site educațional',
 		'meta_keywords' => 'Educație',
 		'author' => 'Entropy Crusade',
-		'logo' => 'Moxie',
+		'logo' => 'M<img src = "assets/img/Moxie.svg" width= 30 height=30>xie',
 		'home' => 'Acasă',
-		'about' => 'Despre noi',
+		'about' => 'Despre Noi',
 		'about_content' => 'Noi suntem Entropy Crusade, o echipă formată din 5 tineri ce vor să aducă o schimbare în domeniul educaţiei. Scopul nostru principal este să ajutăm la digitalizarea acesteia şi să schimbăm felul în care elevii văd şcoala; să o aducem la pragul de ceva făcut cu plăcere, nu doar din obligaţie. Moxie, platforma noastră, este primul pas prin care ne dorim să ne atingem ţelul. 
 		Suntem încă la început de drum, motiv pentru care platforma este încă în dezvoltare şi continua creştere. De aceea, ne cerem scuze pentru eventualele lipsuri sau erori. 
 		În cazul în care aveţi întrebări, nemulţumiri sau sugestii, vă rugăm să ne contactaţi la adresa de email de mai jos. Vă mulţumim!',
@@ -16,7 +16,7 @@
 		'chinese' => 'Chinese',
 		'copy_right' => '© 2021, All rights Reserved',
 		'developed_by' => 'Dezvoltat de',
-		'read_more' => 'Citește mai mult',
+		'read_more' => 'Citește în continuare',
 		'admin_logo' => 'Panou de control',
 		'categories' => 'Categorii',
 		'posts' => 'Postări',
@@ -24,24 +24,24 @@
 		'logout' => 'Deconectare',
 		'login_title' => 'Conectează-te aici',
 		'username' => 'Nume de utilizator',
-		'password' => 'Parola',
+		'password' => 'Parolă',
 		'btn_login' => 'Conectare',
-		'login_success' => 'Conectat cu succes',
+		'login_success' => 'Conectat cu succes!',
 		'login_fail' => 'Conectarea a eșuat!',
-		'welcome' => 'Bine ați venit la sistemul de gestionare al conținutului Moxie',
+		'welcome' => 'Welcome to Moxie Content Management System',
 		'welcome_message' => 'Acesta este un simplu CMS.',
-		'add' => 'Adaugă nou',
-		'edit' => 'Editare',
+		'add' => 'Adaugă',
+		'edit' => 'Editează',
 		'delete' => 'Șterge',
 		'sn' => 'S.N.',
 		'title' => 'Titlu',
 		'description' => 'Descriere',
 		'category' => 'Categorie',
 		'is_active' => 'Este activ?',
-		'include_in_menu' => 'Include in meniu?',
+		'include_in_menu' => 'Include în meniu?',
 		'is_featured' => 'Este recomandat?',
 		'actions' => 'Acțiuni',
-		'full_name' => 'Nume complet',
+		'full_name' => 'Numele complet',
 		'email' => 'Email',
 		'edit_category' => 'Editează Categorie',
 		'edit_post' => 'Editează Postare',
@@ -51,33 +51,115 @@
 		'add_user' => 'Adaugă Utilizator',
 		'yes' => 'Da',
 		'no' => 'Nu',
-		'add_success' => 'Adăugat cu succes',
-		'add_fail' => 'Adăugarea a eșuat',
+		'add_success' => 'Adăugat cu succes!',
+		'add_fail' => 'Adăugarea a eșuat!',
 		'add_fail_email' => 'Email invalid',
-		'add_fail_password' => 'Parola trebuie să aibă minim 6 caractere',
-		'add_fail_password_not_match' => 'Parolele nu coincid',
-		'add_fail_lastname' => 'Nume de familie invalid',
-		'add_fail_firstname' => 'Prenume invalid',
-		'add_fail_initiala' => 'Inițiala tatălui invalidă',
-		'edit_success' => 'Actualizat cu succes',
+		'add_fail_password' => 'Parola trebuie să conțină măcar 6 caractere',
+		'add_fail_password_not_match' => 'Parolele nu se potrivesc',
+		'add_fail_lastname' => 'Numele de familie nu este valid',
+		'add_fail_firstname' => 'Prenumele nu este valid',
+		'add_fail_initiala' => "Inițiala tatălui nu este validă",
+		'edit_success' => 'Actualizat cu succes!',
 		'edit_fail' => 'Actualizarea a eșuat!',
-		'delete_success' => 'Șters cu succes',
+		'delete_success' => 'Șters cu succes!',
 		'delete_fail' => 'Ștergerea a eșuat!',
 		'profile' => 'Pagină de profil',
 		'courses' => 'Cursuri',
 		'assignments' => 'Sarcini',
 		'digitallib' => 'Librărie digitală',
-		'community' => 'Comunitate',
+		'communityh' => 'Comunitate',
 		'rewards' => 'Recompense',
 		'howtouse' => 'Cum se folosește',
 		'login' => 'Conectare',
-		'signup' => 'Înscriere',
+		'signup' => 'Înregistrare',
 		'welcomeuser' => 'Bun venit, ',
 		'avatar' => 'Avatar',
 		'generate_avatar' => 'Generează Avatar',
 		'signup_success' => 'Înscris cu succes!',
-        'communityh' => 'Fila Comunitate',
-        'namep' => 'Nume:',
-        
+		'howtoh' => 'Cum se folosește',
+		'digitallibh' => 'Librărie digitală',
+		'assignmentsh' => 'Sarcini',
+		'rewardsh' => 'Recompense',
+		'coursesh' => 'Cursuri',
+		'welcomepageh' => 'Bun venit pe Moxie!',
+		'welcomepagemessage' => 'Acest site te va învăța!',
+		'lastlecture' => 'Ultimul curs:',
+		'usernameslashemail' => 'Nume de utilizator/Email',
+		'firstnamep' => 'Prenume:',
+		'familynamep' => 'Nume de familie:',
+		'pointsp' => 'Puncte:',
+		'registrydate' => 'Data înregistrării',
+		'usernamep' => 'Nume de utilizator:',
+		'avatargender' => 'Băiat sau Fată',
+		'avatarmale' => 'Băiat',
+		'avatarfemale' => 'Fată',
+		'avataryes' => 'Da',
+		'avatarno' => 'Nu',
+		'avatareye' => 'O 
+		  				C
+					    H
+						I',
+		'avatarmouth' => 'G
+						  U
+						  R
+						  Ă',
+		'avatarhat' => 'P
+						Ă
+						R',
+		'avatarpants' => 'P
+						  A
+						  N
+						  T
+						  A
+						  L
+						  O
+						  N
+						  I',
+		'avatarbody' => 'T
+						 R
+						 I
+						 C
+						 O
+						 U',
+		'avatarshoes' => 'P
+						  A
+						  N
+						  T
+						  O
+						  F
+						  I',
+		'emailregister' => 'Email',
+		'fatherinitial' => "Inițiala numelui tatălui",
+		'firstnameregister' => 'Prenume',
+		'familynameregister' => 'Nume de familie',
+		'repeatpassword' => 'Repetă Parola',
+		'gdpr' => 'Acceptarea politicii de confidențialitate',
+		'registerbutton' => 'Înregistrare',
+		'notmvp' => 'Asta nu este inclus în MVP',
+		'dashboardh' => 'Bord',
+		'avatarstyle' => 'Stil: ',
+		'save_avatar' => 'Salvează Avatar',
+		'subcategory' => 'Subcategorie',
+		'romanian' => 'Română',
+		'subtitle' => 'Sub Categorie',
+		'search' => 'Căutare',
+		'generalstyle' => 'Stil ',
+		'erroravatar' => "Nu deții acest obiect! ",
+		'itemowned' => 'Deținut',
+		'btn_buy' => 'Cumpără',
+		'itemnotowned' => 'Nu este deținut',
+		'cost' => ' Cost: ',
+		'itemhair' => 'Elementul pe care nu îl dețineți este Păr.',
+		'itemeyes' => 'Elementul pe care nu îl dețineți este Ochi.',
+		'itemmouth' => 'Elementul pe care nu îl dețineți este Gură.',
+		'itemtshirt' => 'Elementul pe care nu îl dețineți este Tricou.',
+		'itempants' => 'Elementul pe care nu îl dețineți este Pantaloni.',
+		'itemshoes' => 'Elementul pe care nu îl dețineți este Pantofi.',
+		'thismoxie' => 'Aceasta este Moxie!',
+		'thiscrusade' => 'Acesta este logo-ul nostru!',
+		'pct' => ' Stele de mare',
+		'nopoints' => 'Stele de mare insuficiente !',
+		'errorpoints' => 'O problemă a apărut!Oh, nu!',
+		'successpoints' => 'Articolul achiziționat a fost adăugat la selecția dvs. de avatar.'
 	);
 ?>
