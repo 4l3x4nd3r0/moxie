44715ee1338252cabfde284037f576498bd1f916
4l3x4nd3r0 <razvanpanaite21@gmail.com>
2021-05-28 17:03:27 +0300

    Reorganized menu

--- includes/body.php
+++ includes/body.php
@@ -1,22 +1,24 @@
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <?php echo sprintf($format, $site,'home', $lang['home']);?>
-  <?php echo sprintf($format, $site,'about', $lang['about']);?>
   <?php
     if (isset($_SESSION["user"])) {
-		echo sprintf($format, $site,'profile', $lang['profile']);
+		echo sprintf($format, $site,'home', $lang['home']);
 		echo sprintf($format, $site,'courses', $lang['courses']);
 		echo sprintf($format, $site,'assignments', $lang['assignments']);
 		echo sprintf($format, $site,'digitallibrary', $lang['digitallib']);
-		echo sprintf($format, $site,'community', $lang['communityh']);
+		echo sprintf($format, $site,'profile', $lang['profile']);
 		echo sprintf($format, $site,'rewards', $lang['rewards']);
+		echo sprintf($format, $site,'community', $lang['communityh']);
 		echo sprintf($format, $site,'howtouse', $lang['howtouse']);
+		echo sprintf($format, $site,'about', $lang['about']);
 		echo sprintf($format, $site,'logout', $lang['logout']);
 		
     }
     else {
-		echo sprintf($format, $site,'login', $lang['login']);
+		echo sprintf($format, $site,'home', $lang['home']);
+		echo sprintf($format, $site,'about', $lang['about']);
 		echo sprintf($format, $site,'signup', $lang['signup']);
+		echo sprintf($format, $site,'login', $lang['login']);
     }
 	?>
 </div>
